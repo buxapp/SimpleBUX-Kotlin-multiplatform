d852c05649c2adc7608f6cef659b66944512ccef
Balazs Toth <toth.balazs@me.com>
2021-05-06 22:41:53 +0200

    Add basic product list for iOS

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,10 +1,53 @@
+import Combine
 import SwiftUI
 import shared
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world")
+class ContentViewModel: ObservableObject {
+    @Published var products: [Product] = []
+
+    init(
+        repository: BuxRepository = BuxRepository()
+    ) {
+        self.repository = repository
     }
+
+    func onAppear() {
+        fetchProducts()
+    }
+
+    private func fetchProducts() {
+        repository.fetchProducts { [weak self] (product, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+
+            guard let products = product else {
+                print("Unexpected error when unwrapping products")
+                return
+            }
+
+            self?.products = products
+        }
+    }
+
+    private var repository: BuxRepository
 }
 
+struct ContentView: View {
+    @ObservedObject var viewModel: ContentViewModel
 
+    var body: some View {
+        NavigationView {
+            List(viewModel.products, id: \.self) { product in
+                HStack {
+                    Text(product.displayName)
+                    Spacer()
+                    Text(product.currentPrice.amount.description)
+                }
+            }
+            .navigationTitle("Products")
+        }
+        .onAppear(perform: viewModel.onAppear)
+    }
+}
