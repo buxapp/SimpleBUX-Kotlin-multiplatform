f39c320f1363337fc9c229f09fc28253496a07a9
Nikos Kermanidis <nikos.kermanidis@getbux.com>
2021-05-06 15:08:25 +0200

    Bump Android compileSdkVersion and targetSdkVersion to 30

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -44,7 +44,7 @@ kotlin {
         val androidTest by getting {
             dependencies {
                 implementation(kotlin("test-junit"))
-                implementation("junit:junit:4.12")
+                implementation("junit:junit:4.13.2")
             }
         }
         val iosMain by getting
@@ -52,11 +52,11 @@ kotlin {
     }
 }
 android {
-    compileSdkVersion(29)
+    compileSdkVersion(30)
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
     defaultConfig {
         minSdkVersion(24)
-        targetSdkVersion(29)
+        targetSdkVersion(30)
         versionCode = 1
         versionName = "1.0"
     }
